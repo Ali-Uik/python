5effb0357f6cf229f69e967d1772c697525f2187
unknown <izzatbek1987@gmail.com>
2021-09-13 19:16:17 +0500

    admin

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us' # настройка языка
+LANGUAGE_CODE = 'ru'           # настройка языка (для английского языка en-us)
 
 TIME_ZONE = 'UTC'
 
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News     # импорт моделей
 
 
 def index(request):
-    news = News.objects.order_by('-created_at')
+    news = News.objects.all()
     context = {
         'news': news,
         'title': 'Список новостей'
