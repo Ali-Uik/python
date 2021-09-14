4646a903033dfdac089eba746bdde21a579ea23e
unknown <izzatbek1987@gmail.com>
2021-09-14 18:13:14 +0500

    Model relationships

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
+
 class News(models.Model):
+
     title = models.CharField(max_length=150, verbose_name='Наименование')
     content = models.TextField(blank=True, verbose_name='Контент')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото')   # для работы с ImageField мы должны установить библиотеку Piсв llow
-    is_published = models.BooleanField(default=True, verbose_name='Опубликовано?')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)   # для работы с
+    # ImageField мы должны установить библиотеку Pillow
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')   # модель
+    # News соединяем с первичным моделем Category
 
     def __str__(self):
         return self.title
@@ -15,3 +20,15 @@ class News(models.Model):
         verbose_name = "Новость"
         verbose_name_plural = "Новости"
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ['title']
